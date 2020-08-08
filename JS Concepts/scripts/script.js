624928188593aa16cd122f3ae29362935133546e
Nur A Alam <nuraalam376@gmail.com>
2020-08-08 06:14:13 +0600

    practised json api

---
+++ JS Concepts/scripts/script.js
@@ -0,0 +1,11 @@
+// Single Line Comment
+/*
+Multi line comment
+
+*/
+console.log("Connect Js Files to HTML");
+console.log("Single quote inside 'double quote'");
+console.log('Double quote inside "single quote"');
+
+var name = prompt("Enter your name: ");
+document.write(name);
---
+++ JS Concepts/variable.js
@@ -0,0 +1,23 @@
+var a = 10;
+var b = a;
+console.log(a, b);
+
+a = 20;
+console.log(a, b);
+
+var a;
+console.log(a);
+
+let $a, _a, abc123;
+console.log($a, _a, abc123);
+
+let name = 12;
+console.log(name);
+name = 33.22;
+console.log(name);
+name = "Abc";
+console.log(name);
+
+const abc = 10;
+// abc = 20;
+console.log(abc);
