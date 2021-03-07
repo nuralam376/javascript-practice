6cb149d9d83bec6a75e7474fa84bf337f7fe5ad0
Nur A Alam <nuraalam376@gmail.com>
2021-03-07 22:30:45 +0600

    practiced js hoisting

---
+++ js-advanced/scopes/hoisting.js
@@ -0,0 +1,20 @@
+//
+
+// let LANGUAGE = "Java";
+// let language = "JavaScript";
+
+// function getLanguage() {
+//   if (!language) {
+//     let language = LANGUAGE;
+//   }
+//   return language;
+// }
+
+// console.log(`I love ${getLanguage()}`);
+
+myFunc();
+function myFunc() {
+  language = "JS";
+  var language;
+  console.log(language);
+}
