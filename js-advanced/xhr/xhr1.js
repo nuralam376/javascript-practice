fcfd418470508803af30d2f42a437b610423b82b
Nur A Alam <nuraalam376@gmail.com>
2021-03-17 08:56:36 +0600

    practiced js xhr

--- js-advanced/xhr/xhr1.js
+++ js-advanced/xhr/xhr1.js
@@ -1,9 +1,53 @@
 const getData = document.querySelector("#get-data");
+const sendData = document.querySelector("#send-data");
+
+const sendRequest = (method, url, data) => {
+  const promise = new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.responseType = "json";
+    xhr.setRequestHeader("Content-type", "application/json");
+    xhr.send();
+
+    xhr.onload = function () {
+      if (xhr.status >= 400) {
+        reject("Not");
+      }
+      resolve(xhr.response);
+    };
+
+    xhr.onerror = function () {
+      reject("Something was wrong");
+    };
+  });
+  return promise;
+};
 
 const getDataFromServer = () => {
-  const xhr = new XMLHttpRequest();
-  xhr.open("GET", "https://jsonplaceholder.typicode.com/users");
-  xhr.send();
+  sendRequest("GET", "https://jsonplaceholder.typicode.com/users/1")
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+const sendDataToServer = () => {
+  sendRequest(
+    "POST",
+    "https://jsonplaceholder.typicode.com/users",
+    JSON.stringify({
+      name: "Abc",
+    })
+  )
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
 getData.addEventListener("click", getDataFromServer);
+sendData.addEventListener("click", sendDataToServer);
