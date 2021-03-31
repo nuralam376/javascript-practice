1b078c1d489b8e3e0d095d830b2d5db731169edd
Nur A Alam <nuraalam376@gmail.com>
2021-03-31 07:55:47 +0600

    practiced js dom

--- todo-app-with-dom/script.js
+++ todo-app-with-dom/script.js
@@ -26,11 +26,42 @@ const addTask = function (event) {
   bindIncompleteList(listItem, completeTask);
 };
 
-const completeTask = function () {};
+const completeTask = function () {
+  const listItem = this.parentNode;
+  const deleteBtn = document.createElement("button");
+  deleteBtn.className = "delete";
+  deleteBtn.innerText = "Delete";
+  listItem.appendChild(deleteBtn);
+
+  completeUl.appendChild(listItem);
+
+  const checkbox = listItem.querySelector("input[type='checkbox']");
+  checkbox.remove();
+
+  bindCompleteList(listItem, deleteTask);
+};
+
+const deleteTask = function () {
+  const listItem = this.parentNode;
+  listItem.remove();
+};
 
 const bindIncompleteList = function (listItem, checkboxClick) {
   const checkbox = listItem.querySelector("input[type='checkbox']");
   checkbox.onchange = checkboxClick;
 };
 
+const bindCompleteList = (listItem, deleteBtnClick) => {
+  const deleteBtn = listItem.querySelector(".delete");
+  deleteBtn.onclick = deleteBtnClick;
+};
+
 form.addEventListener("submit", addTask);
+
+for (let i = 0; i < todoUl.children.length; i++) {
+  bindIncompleteList(todoUl.children[i], completeTask);
+}
+
+for (let i = 0; i < completeUl.children.length; i++) {
+  bindCompleteList(completeUl.children[i], deleteTask);
+}
